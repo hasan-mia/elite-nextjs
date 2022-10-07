2da00b5f888f1537559afc8609e802125d8a1675
Md. Hasan Mia <hasanrafi69@gmail.com>
2022-10-07 15:32:10 +0600

    load data from api

--- components/Cards.js
+++ components/Cards.js
@@ -1,43 +1,58 @@
 import Image from 'next/image';
-import room from "../assets/room.png";
-const Cards = ({ data }) => {
+import { useEffect, useState } from 'react';
+import room from '../assets/room.png'
+
+// export const getStaticProps = async () => {
+//     const res = await fetch("https://raw.githubusercontent.com/hasan-mia/hasan-mia/main/data.json");
+//     const data = await res.json();
+//     return {
+//         props: {
+//             data,
+//         },
+//     };
+// };
+
+const Cards = () => {
+    const [data, setData] = useState([]);
+    const dataLoad = () => {
+        const url = 'https://raw.githubusercontent.com/hasan-mia/hasan-mia/main/data.json';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setData(data))
+    }
+    useEffect(() => {
+        dataLoad()
+    }, [])
+    console.log(data);
     return (
-        <div className="card bg-white rounded-md w-72 border border-gray-300">
-            <div className="card-header pb-2">
-                <a href="#" blur-shadow-image="true">
-                    <Image className="w-auto rounded-lg" src={room} alt="card image" />
-                </a>
-            </div>
-            <div className="card-body px-4">
-                <a href="#">
-                    <h5 className="text-lg text-black">Restauran Name</h5>
-                </a>
-                <p className="my-2 text-gray-700">
-                    <i className="far fa-map-marker-alt"></i>
-                    <span className="ml-3">USA</span>
-                </p>
-                <p className="my-2 text-gray-700">
-                    <i className="fal fa-eye"></i>
-                    <span className="ml-2">25k people visited</span>
-                </p>
-            </div>
-        </div>
+        <>
+            {
+                data?.slice(0, 3).map(item =>
+                    <div key={item.id} className="card bg-white rounded-md w-72 border border-gray-300">
+                        <div className="card-header pb-2">
+                            <a href="#" blur-shadow-image="true">
+                                {/* <Image src={item.img} layout='fill' alt="image" /> */}
+                            </a>
+                        </div>
+                        <div className="card-body px-4">
+                            <a href="#">
+                                <h5 className="text-lg text-black">{item.title}</h5>
+                            </a>
+                            <p className="my-2 text-gray-700">
+                                <i className="far fa-map-marker-alt"></i>
+                                <span className="ml-3">{item.location}</span>
+                            </p>
+                            <p className="my-2 text-gray-700">
+                                <i className="fal fa-eye"></i>
+                                <span className="ml-2">{item.view}</span>
+                            </p>
+                        </div>
+                    </div>
+                )
+            }
+
+        </>
     );
 };
-
-export async function getServerSideProps(context) {
-    // Call an external API endpoint to get posts.
-    // fetching data
-    const res = await fetch('../public/data/data.json')
-    const data = await res.json()
-   data.map(item=>console.log(item))
-
-    // receive `datas` as a prop at build time
-    return {
-        props: {
-            data,
-        },
-    }
-}
 
 export default Cards;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['github.com','ibb.co', 'githubusercontent.com'],
+    formats: ['image/avif', 'image/webp', 'image/png', 'image/jpeg', 'image/jpg'],
+  },
   reactStrictMode: true,
   swcMinify: true,
+  // experimental: { esmExternals: true },
 }
 
 module.exports = nextConfig
